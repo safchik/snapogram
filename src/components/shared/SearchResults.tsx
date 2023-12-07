cef02a78622fdcb20710a72c6a32cc0c6e18e0b6
safchik <safchik9@gmail.com>
2023-12-07 19:31:16 +0000

    search results component logic added

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,7 +1,22 @@
+import { Models } from "appwrite";
+import { Loader } from "lucide-react";
+import GridPostList from "./GridPostList";
+import { searchPosts } from "@/lib/appwrite/api";
 
-const SearchResults = () => {
+type SearchResultsProps = {
+    isSearchFetching: boolean;
+    searchedPosts: Models.Document[];
+}
+
+const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultsProps) => {
+    if (isSearchFetching) return <Loader />
+    if (searchedPosts && searchedPosts.documents.length > 0) {
+        return (
+            <GridPostList posts={searchPosts.documents} />
+        )
+    }
     return (
-        <div>SearchResults</div>
+        <p className="text-light-4 mt-10 text-center w-full">No results found.</p>
     )
 }
 
