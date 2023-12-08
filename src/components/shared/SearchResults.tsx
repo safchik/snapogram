b8bfd93a4b78e01fa733cd0aef9de38a17816a16
safchik <safchik9@gmail.com>
2023-12-08 21:45:13 +0000

    working on deployment

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,23 +1,19 @@
 import { Models } from "appwrite";
 import { Loader } from "lucide-react";
 import GridPostList from "./GridPostList";
-import { searchPosts } from "@/lib/appwrite/api";
+
 
 type SearchResultsProps = {
     isSearchFetching: boolean;
-    searchedPosts: Models.Document[];
-}
+    searchedPosts: Models.DocumentList<Models.Document> | undefined;
+};
 
 const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultsProps) => {
-    if (isSearchFetching) return <Loader />
+    if (isSearchFetching) return <Loader />;
     if (searchedPosts && searchedPosts.documents.length > 0) {
-        return (
-            <GridPostList posts={searchPosts.documents} />
-        )
+        return <GridPostList posts={searchedPosts.documents} />;
     }
-    return (
-        <p className="text-light-4 mt-10 text-center w-full">No results found.</p>
-    )
-}
+    return <p className="text-light-4 mt-10 text-center w-full">No results found.</p>;
+};
 
-export default SearchResults
+export default SearchResults;
