f477fb6930dcd258bc6c797717e57c5ed0610198
safchik <safchik9@gmail.com>
2023-12-08 21:48:02 +0000

    working on deployment

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -2,7 +2,6 @@ import { Models } from "appwrite";
 import { Loader } from "lucide-react";
 import GridPostList from "./GridPostList";
 
-
 type SearchResultsProps = {
     isSearchFetching: boolean;
     searchedPosts: Models.DocumentList<Models.Document> | undefined;
